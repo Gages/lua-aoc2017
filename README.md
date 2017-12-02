abb860225633239bd4dadfca6f02ae76c019a4e1
Phillip Warren <phillip.warren@gmail.com>
2017-12-02 17:20:14 +1100

    Some reformatting and additional utility functions

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # lua-aoc2017
 
-These are my solutions to Advent of Code 2017.
+These are my solutions to [Advent of Code 2017](https://adventofcode.com/).
 
-I have used Lua 5.1 with just the standard libraries.
+I have used [Lua 5.1](https://www.lua.org/manual/5.1/manual.html) with just the standard libraries.
--- common/init.lua
+++ common/init.lua
@@ -25,19 +25,51 @@ function m.map(f, t)
 end
 
 function m.min(t)
-  local a = t[1]
-  for i=2, #t do
-    a = math.min(a, t[i])
-  end
-  return a
+  return m.fold_offset(math.min, t, t[1], 2)
 end
 
 function m.max(t)
-  local a = t[1]
-  for i=2, #t do
-    a = math.max(a, t[i])
+  return m.fold_offset(math.max, t, t[1], 2)
+end
+
+function m.fold_offset(f, t, acc, i)
+  if i > #t then
+    return acc
+  else
+    return m.fold_offset(f, t, f(acc, t[i]), i+1)
   end
-  return a  
+end
+
+function m.fold(f, t, init)
+  return m.fold_offset(f, t, init, 1)
+end
+
+function m.add(acc, first, ...)
+  if first then
+    return m.add(acc + first, ...)
+  else
+    return acc
+  end
+end
+
+function m.sum(t)
+  return m.fold(m.add, t, 0)
+end
+
+--compose : (X -> Y) -> (Y -> Z) -> (X -> Z)
+function m.compose(f, g)
+  return function(...)
+    return g(f(...))
+  end
+end
+
+function m.range(start, stop, step)
+  local step = step or 1
+  local acc = {}
+  for i=start,stop,step do
+    table.insert(acc, i)
+  end
+  return acc
 end
 
 return m
